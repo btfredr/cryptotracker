b27490c314d1d7c53db7d34ec5856bb05144586b
Fredrik <fredrikf2000@gmail.com>
2021-10-16 14:22:21 +0200

    fetched currencies from API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useState, useEffect } from "react";
 import Currency from "./components/Currency";
 import { BASE_URL } from "./utils/constants";
 
 function App() {
-  const [currencies, setCurrencies] = useState([]);
+  const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
       .get(BASE_URL)
-      .then((response) => {
-        setCurrencies(response.data);
-        console.log(response.data);
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -21,32 +21,34 @@ function App() {
     setSearch(e.target.value);
   };
 
-  const filteredCurrencies = currencies.filter((currency) => {
-    currency.name.toLowerCase().includes(search.toLowerCase());
-  });
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="cryptoApp">
       <div className="cryptoApp__search">
-        <h1 className="cryptoApp__h1">Search a currency</h1>
+        <h1 className="cryptoApp__text">Search a currency</h1>
         <form>
           <input
-            type="text"
-            placeholder="Search"
             className="cryptoApp__input"
+            type="text"
             onChange={handleChange}
+            placeholder="Search"
           />
         </form>
       </div>
-      {filteredCurrencies.map((currency) => {
+      {filteredCoins.map((coin) => {
         return (
           <Currency
-            key={currency.id}
-            name={currency.name}
-            price={currency.current_price}
-            symbol={currency.symbol}
-            image={currency.image}
-            volume={currency.market_cap}
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
           />
         );
       })}
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,15 +1,35 @@
-const Currency = ({ name, image, symbol, price, volume }) => {
+import React from "react";
+
+const Currency = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
   return (
     <div className="currency">
-      <div className="currency__row">
+      <div className="currency-row">
         <div className="currency__item">
           <img src={image} alt="crypto" />
           <h1>{name}</h1>
-          <p className="currency__symbol">{symbol}</p>
+          <p className="currency-symbol">{symbol}</p>
         </div>
         <div className="currency__data">
           <p className="currency__price">{price} NOK</p>
           <p className="currency__volume">{volume.toLocaleString()} NOK</p>
+
+          {priceChange < 0 ? (
+            <p className="currency__percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="currency__percent green">{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className="currency__marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
